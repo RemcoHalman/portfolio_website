c37874b59ecb66eedfb6a2d9e7b75e66ccc96839
RemcoHalman <remco.halman@gmail.com>
2020-10-09 12:44:54 +0200

    add project image closes #1

--- assets/js/vue.js
+++ assets/js/vue.js
@@ -1,5 +1,5 @@
 Vue.component("portfolio", {
-    template: `
+  template: `
     <div>
     <div class="row">
         <div v-for="(project, index) in projects" class="col-md-6 col-lg-3 mb-5">
@@ -107,152 +107,153 @@ Vue.component("portfolio", {
     </div>
                 
     `,
-    data() {
-        return {
-            projects: [
-                {
-                    projectId: 1,
-                    projectName: "Escaperoom Drachten",
-                    projectImage:
-                        "/assets/img/portfolio/websites/escaperoomdrachten-nl-2020-09-08-14_41_24.png",
-                    projectTags: ["html 5", "css", "Wordpress"],
-                    projectDescription:
-                        "Nieuwe website Escaperoom Drachten. Met extra Custom Post Types om gemakkelijk een nieuwe kamer toe te voegen",
-                    projectLink: "https://www.escaperoomdrachten.nl/",
-                },
-                {
-                    projectId: 2,
-                    projectName: "Timmerbedrijf R Wynia",
-                    projectImage:
-                        "/assets/img/portfolio/websites/rwynia-nl-2020-09-08-14_41_44.png",
-                    projectTags: ["html 5", "css", "Wordpress"],
-                    projectDescription:
-                        "Website van Timmerbedrijf R Wynia om online extra vindbaar te zijn",
-                    projectLink: "https://www.rwynia.nl/",
-                },
-                {
-                    projectId: 3,
-                    projectName: "Emmelocked",
-                    projectImage: "/assets/img/portfolio/leds_emmelocked.gif",
-                    projectTags: ["C++", "Arduino"],
-                    projectDescription:
-                        "Licht Effect in Escaperoom Emmelocked.",
-                },
-                {
-                    projectId: 4,
-                    projectName: "Escaperoom Effect",
-                    projectImage: "/assets/img/portfolio/clock_effect.gif",
-                    projectTags: ["C++"],
-                    projectDescription: "Escaperoom Drachten doorgang effect",
-                },
-                {
-                    projectId: 5,
-                    projectName: "WebApp to send photo",
-                    projectImage: "/assets/img/portfolio/photo_mailer.jpg",
-                    projectTags: ["html 5", "css", "Flask", "Python"],
-                    projectDescription:
-                        "Web app om foto's te verzenden naar gasten. Inclusief een leuke mail.",
-                },
-                {
-                    projectId: 6,
-                    projectName: "WebApp keep track of work hours",
-                    projectImage:
-                        "/assets/img/portfolio/escaperoomdrachten_uren.jpg",
-                    projectTags: ["html 5", "css", "Django", "Python"],
-                    projectDescription:
-                        "Web app Escaperoom Drachten om de uren registratie van personeel bij te houden. Het personeel werkt steeds meer zelfstandig en zo kan iedereen makkelijk zien wat de planning is.",
-                },
-                {
-                    projectId: 7,
-                    projectName: "Control of clues send in the escaperoom",
-                    projectImage: "/assets/img/portfolio/clue_control.jpg",
-                    projectTags: ["PyQt5", "Python"],
-                    projectDescription:
-                        "GUI voor The Lost Ones. Hints kunnen zo makkelijk getypt worden en laten zien in de kamer",
-                },
-                {
-                    projectId: 8,
-                    projectName: "Control of escaperoom",
-                    projectImage: "/assets/img/portfolio/automation.jpeg",
-                    projectTags: ["Node", "javascript"],
-                    projectDescription:
-                        "PLC gebaseerd op een Raspberry Pi en een online Portaal om de acties te besturen",
-                },
-                {
-                    projectId: 9,
-                    projectName: "Escaperoom Game",
-                    projectImage: "/assets/img/portfolio/buttongame.jpeg",
-                    projectTags: ["C++", "Arduino"],
-                    projectDescription: "Fysieke escaperoom game",
-                },
-                {
-                    projectId: 10,
-                    projectName: "Todo App",
-                    projectImage: "/assets/img/portfolio/todo.jpg",
-                    projectTags: ["html 5", "css", "Django", "Python"],
-                    projectDescription: "TODO app",
-                },
-                {
-                    projectId: 11,
-                    projectName: "Watermarking Images",
-                    projectImage: "/assets/img/portfolio/watermarked.jpg",
-                    projectTags: ["PyQt5", "Python", "Pillow"],
-                    projectDescription:
-                        "Desktop app om het makkelijker te maken om foto's te watermerken. Het gaat per volledige folder",
-                },
-                {
-                    projectId: 12,
-                    projectName: "Escaperoom game",
-                    projectImage: "/assets/img/portfolio/veet_effect.jpeg",
-                    projectTags: ["C++", "Python"],
-                    projectDescription:
-                        "Licht effect voor een Game in The Lost Ones",
-                },
-                {
-                    projectId: 13,
-                    projectName: "Escaperoom game",
-                    projectImage:
-                        "/assets/img/portfolio/touchscreen_js_game.jpg",
-                    projectTags: ["Javascript", "Html", "Css"],
-                    projectDescription:
-                        "Touchscreen spel. Bezoekers moeten het juiste woord invoeren om verder te kunnen",
-                },
-                {
-                    projectId: 15,
-                    projectName: "Vue Monster Slayer",
-                    projectImage:
-                        "/assets/img/portfolio/vue_monster_slayer.jpg",
-                    projectTags: ["Vue", "Javascript", "Html", "Css"],
-                    projectDescription: "Simpel online click game",
-                    projectLink: "https://remcohalman.nl/monster-slayer/",
-                },
-                {
-                    projectId: 14,
-                    projectName: "Holiday Countdown",
-                    projectImage:
-                        "/assets/img/portfolio/websites/holiday_countdown.jpg",
-                    projectTags: ["Javascript", "VueJS", "Html", "SCSS"],
-                    projectDescription:
-                        "Een countdown tot de komende feestdagen.",
-                    projectLink: "https://www.remcohalman.nl/countdown",
-                }, 
-                {
-                    projectId: 16,
-                    projectName: "Vue versie van community website (vuejs branch)",
-                    projectImage:
-                        "/assets/img/portfolio/websites/holiday_countdown.jpg",
-                    projectTags: ["Javascript", "VueJS", "Html", "SCSS", "CSS"],
-                    projectDescription:
-                        "Een rebuild van een community website. Om verschillende frameworks te vergelijken",
-                    projectLink: "https://github.com/EddieJaoudeCommunity/EddieJaoudeCommunity.github.io"
-                }, 
-            ],
-        };
-    },
+  data() {
+    return {
+      projects: [
+        {
+          projectId: 1,
+          projectName: "Escaperoom Drachten",
+          projectImage:
+            "/assets/img/portfolio/websites/escaperoomdrachten-nl-2020-09-08-14_41_24.png",
+          projectTags: ["html 5", "css", "Wordpress"],
+          projectDescription:
+            "Nieuwe website Escaperoom Drachten. Met extra Custom Post Types om gemakkelijk een nieuwe kamer toe te voegen",
+          projectLink: "https://www.escaperoomdrachten.nl/",
+        },
+        {
+          projectId: 2,
+          projectName: "Timmerbedrijf R Wynia",
+          projectImage:
+            "/assets/img/portfolio/websites/rwynia-nl-2020-09-08-14_41_44.png",
+          projectTags: ["html 5", "css", "Wordpress"],
+          projectDescription:
+            "Website van Timmerbedrijf R Wynia om online extra vindbaar te zijn",
+          projectLink: "https://www.rwynia.nl/",
+        },
+        {
+          projectId: 3,
+          projectName: "Emmelocked",
+          projectImage: "/assets/img/portfolio/leds_emmelocked.gif",
+          projectTags: ["C++", "Arduino"],
+          projectDescription: "Licht Effect in Escaperoom Emmelocked.",
+        },
+        {
+          projectId: 4,
+          projectName: "Escaperoom Effect",
+          projectImage: "/assets/img/portfolio/clock_effect.gif",
+          projectTags: ["C++"],
+          projectDescription: "Escaperoom Drachten doorgang effect",
+        },
+        {
+          projectId: 5,
+          projectName: "WebApp to send photo",
+          projectImage: "/assets/img/portfolio/photo_mailer.jpg",
+          projectTags: ["html 5", "css", "Flask", "Python"],
+          projectDescription:
+            "Web app om foto's te verzenden naar gasten. Inclusief een leuke mail.",
+        },
+        {
+          projectId: 6,
+          projectName: "WebApp keep track of work hours",
+          projectImage: "/assets/img/portfolio/escaperoomdrachten_uren.jpg",
+          projectTags: ["html 5", "css", "Django", "Python"],
+          projectDescription:
+            "Web app Escaperoom Drachten om de uren registratie van personeel bij te houden. Het personeel werkt steeds meer zelfstandig en zo kan iedereen makkelijk zien wat de planning is.",
+        },
+        {
+          projectId: 7,
+          projectName: "Control of clues send in the escaperoom",
+          projectImage: "/assets/img/portfolio/clue_control.jpg",
+          projectTags: ["PyQt5", "Python"],
+          projectDescription:
+            "GUI voor The Lost Ones. Hints kunnen zo makkelijk getypt worden en laten zien in de kamer",
+        },
+        {
+          projectId: 8,
+          projectName: "Control of escaperoom",
+          projectImage: "/assets/img/portfolio/automation.jpeg",
+          projectTags: ["Node", "javascript"],
+          projectDescription:
+            "PLC gebaseerd op een Raspberry Pi en een online Portaal om de acties te besturen",
+        },
+        {
+          projectId: 9,
+          projectName: "Escaperoom Game",
+          projectImage: "/assets/img/portfolio/buttongame.jpeg",
+          projectTags: ["C++", "Arduino"],
+          projectDescription: "Fysieke escaperoom game",
+        },
+        {
+          projectId: 10,
+          projectName: "Todo App",
+          projectImage: "/assets/img/portfolio/todo.jpg",
+          projectTags: ["html 5", "css", "Django", "Python"],
+          projectDescription: "TODO app",
+        },
+        {
+          projectId: 11,
+          projectName: "Watermarking Images",
+          projectImage: "/assets/img/portfolio/watermarked.jpg",
+          projectTags: ["PyQt5", "Python", "Pillow"],
+          projectDescription:
+            "Desktop app om het makkelijker te maken om foto's te watermerken. Het gaat per volledige folder",
+        },
+        {
+          projectId: 12,
+          projectName: "Escaperoom game",
+          projectImage: "/assets/img/portfolio/veet_effect.jpeg",
+          projectTags: ["C++", "Python"],
+          projectDescription: "Licht effect voor een Game in The Lost Ones",
+        },
+        {
+          projectId: 13,
+          projectName: "Escaperoom game",
+          projectImage: "/assets/img/portfolio/touchscreen_js_game.jpg",
+          projectTags: ["Javascript", "Html", "Css"],
+          projectDescription:
+            "Touchscreen spel. Bezoekers moeten het juiste woord invoeren om verder te kunnen",
+        },
+        {
+          projectId: 15,
+          projectName: "Vue Monster Slayer",
+          projectImage: "/assets/img/portfolio/vue_monster_slayer.jpg",
+          projectTags: ["Vue", "Javascript", "Html", "Css"],
+          projectDescription: "Simpel online click game",
+          projectLink: "https://remcohalman.nl/monster-slayer/",
+        },
+        {
+          projectId: 14,
+          projectName: "Holiday Countdown",
+          projectImage: "/assets/img/portfolio/websites/holiday_countdown.jpg",
+          projectTags: ["Javascript", "VueJS", "Html", "SCSS"],
+          projectDescription: "Een countdown tot de komende feestdagen.",
+          projectLink: "https://www.remcohalman.nl/countdown",
+        },
+        {
+          projectId: 16,
+          projectName: "Vue versie van community website (vuejs branch)",
+          projectImage:
+            "/assets/img/portfolio/websites/eddiejaoudecommunity-github-io-2020-10-09-12_35_03.png",
+          projectTags: [
+            "Javascript",
+            "VueJS",
+            "Html",
+            "SCSS",
+            "CSS",
+            "cypress",
+          ],
+          projectDescription:
+            "Een rebuild van een community website. Om verschillende frameworks te vergelijken. Cypress word gebruikt om te testen",
+          projectLink:
+            "https://github.com/EddieJaoudeCommunity/EddieJaoudeCommunity.github.io",
+        },
+      ],
+    };
+  },
 });
 
 var app = new Vue({
-    el: "#app",
-    data: {},
-    methods: {},
+  el: "#app",
+  data: {},
+  methods: {},
 });
