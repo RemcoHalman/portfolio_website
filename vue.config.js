1bdea59d00340b092796f9ab223b28ea0fc13d3a
RemcoHalman <remco.halman@gmail.com>
2021-02-03 20:48:50 +0100

    Feat: Deploy initial project

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,5 @@
 // vue.config.js file to be place in the root of your repository
-const path = require("path");
 
 module.exports = {
-  outputDir: path.resolve(__dirname, "gh-pages"),
-  filenameHashing: false,
+  publicPath: "/portfolio_website/",
 };
