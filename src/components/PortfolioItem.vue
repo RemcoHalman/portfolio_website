03de938eaee7c6547dace08d4e36eee9f07cb9b6
RemcoHalman <remco.halman@gmail.com>
2021-02-03 19:59:00 +0100

    Feat: Added portfolio modal

---
+++ src/components/PortfolioItem.vue
@@ -0,0 +1,22 @@
+<template>
+  <div
+    :style="{
+      backgroundImage: 'url(' + image + ')',
+      backgroundSize: 'fit-content',
+    }"
+  >
+    <h2>{{ name }}</h2>
+    <p>{{ description }}</p>
+    <button>Modal</button>
+  </div>
+
+  <!-- tags, link -->
+</template>
+
+<script>
+export default {
+  props: ["id", "name", "description", "image", "tags", "link"],
+};
+</script>
+
+<style lang="scss" scoped></style>
--- src/views/Portfolio.vue
+++ src/views/Portfolio.vue
@@ -1,14 +1,18 @@
 <template>
   <div class="portfolio">
-    <h1>Portfolio</h1>
+    <h1>Portfolio Page</h1>
+    <p>Dit is een greep van alle projecten waar ik aan gewerkt</p>
+    <the-portfolio></the-portfolio>
   </div>
 </template>
 
 <script>
+import ThePortfolio from "@/components/ThePortfolio.vue";
+
 export default {
   name: "Portfolio",
   title: "Remco Halman | Portfolio",
-  components: {},
+  components: { ThePortfolio },
 };
 </script>
 
