4b34fb39d939febeeb8c37975e527df7b3530f4e
RemcoHalman <remco.halman@gmail.com>
2021-02-05 15:00:43 +0100

    Chore: Updated styling

--- src/components/PortfolioItem.vue
+++ src/components/PortfolioItem.vue
@@ -1,16 +1,15 @@
 <template>
   <div
+    class="projectimage"
     :style="{
       backgroundImage: 'url(' + image + ')',
-      backgroundSize: 'fit-content',
     }"
   >
     <h2>{{ name }}</h2>
     <p>{{ description }}</p>
     <button>Modal</button>
+    <!-- tags, link -->
   </div>
-
-  <!-- tags, link -->
 </template>
 
 <script>
@@ -19,4 +18,23 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.projectimage {
+  opacity: 0.5;
+  background-size: cover;
+  background-repeat: no-repeat;
+  &:hover {
+    animation: fadeInFromNone 0.5s ease-in-out;
+  }
+}
+
+@-webkit-keyframes fadeInFromNone {
+  0% {
+    opacity: 0.5;
+  }
+
+  100% {
+    opacity: 1;
+  }
+}
+</style>
