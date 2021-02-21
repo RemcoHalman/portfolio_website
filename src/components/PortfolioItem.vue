843fe47c58bdaa48cb86a0545d831046fd3a0b91
RemcoHalman <remco.halman@gmail.com>
2021-02-21 19:45:50 +0100

    Chore: Added tags

--- src/components/PortfolioItem.vue
+++ src/components/PortfolioItem.vue
@@ -4,6 +4,12 @@
     <h2>- {{ name }} -</h2>
     <h3>-- {{ type }} --</h3>
     <p>{{ description }}</p>
+    <div v-if="tags">
+      <span v-for="tag in tags" :key="tag">
+        {{ tag }}
+      </span>
+    </div>
+    <a :href="link" target="_blank" v-if="link">{{ link }}</a>
   </div>
   <!-- tags, link -->
 </template>
@@ -27,4 +33,16 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+span {
+  margin-left: 10px;
+  background-color: rgba(0, 121, 0, 0.473);
+  border-radius: 5px;
+  padding: 5px;
+}
+
+a {
+  margin: 10px 0 0 0;
+  background-color: rgba(0, 200, 200, 1);
+}
+</style>
